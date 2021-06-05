54b4cd0f561f0689811550933520fa8f6891dd63
AxSch <alex_schuneman@outlook.com>
2021-06-05 13:11:59 +0100

    Add api request functions

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+
+
+const fetchTransportServices = async () => {
+    try {
+        const transportServices = await fetch(`https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr/Status?detail=true`)
+        const data = await transportServices.json()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const fetchBikePoints = async (query) => {
+    try {
+        const bikePoints = await fetch(`https://api.tfl.gov.uk/BikePoint/Search?query=${query}`)
+        const data = await bikePoints.json()
+        return data
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+export {
+    fetchTransportServices,
+    fetchBikePoints
+}
