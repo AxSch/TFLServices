b75ece6dc40bd398aaded879c9867dfa57cdfa49
AxSch <alex_schuneman@outlook.com>
2021-06-05 13:56:16 +0100

    Refactor to use the TFLServices page component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import TFLServices from './Pages/TFLServices';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TFL Service Lookup</h1>
+      <TFLServices />
     </div>
   );
 }
