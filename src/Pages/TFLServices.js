d926030e641151bfa0edbb2c0ecdc617746b3049
AxSch <alex_schuneman@outlook.com>
2021-06-06 22:21:45 +0100

    Refactor to handle loading state
    & handle its initial state

--- src/Pages/TFLServices.js
+++ src/Pages/TFLServices.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { fetchTransportServices } from '../api/api'
 import Menu from '../Components/Menu/Menu'
+import TFLService from '../Components/TFLService/TFLService'
+import { selectCurrentTFLService, setCurrentTFLService } from '../reducers/transportServices/transportServicesSlice'
 import { TFLServicesContainer } from './TFLServices.styled'
 
 const TFLServices = () => {
     const [TFLServiceData, setTFLServiceData] = useState({})
-    
+    const [isLoading, setIsLoading] = useState(true)
+    const currentTFLService = useSelector(selectCurrentTFLService)
+    const dispatch = useDispatch()
+
     useEffect(() => {
         const fetchTFLServices = async () => {
             const newTFLObj = {}
@@ -18,14 +24,32 @@ const TFLServices = () => {
             setTFLServiceData(newTFLObj)
         }
         fetchTFLServices()
-    }, [])
+        setIsLoading(false)
+        dispatch(setCurrentTFLService({}))
+    }, [dispatch])
+
+    const renderServices = () => {
+        if (isLoading) {
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
+        } else {
+            return (
+                <>
+                    <div>
+                        <Menu services={TFLServiceData} />
+                    </div>
+                    <div>{currentTFLService && Object.keys(currentTFLService).length > 0 ? <TFLService service={currentTFLService} /> : null}</div>
+                </>
+            )
+        }
+    }
 
     return (
         <TFLServicesContainer>
-            <div>
-                <Menu services={TFLServiceData} />
-            </div>
-            <div>Results</div>
+            {renderServices()}
         </TFLServicesContainer>
     )
 }
