b3c98fd4712d9a69fd63b497562e6d8b4d0cd61c
AxSch <alex_schuneman@outlook.com>
2021-06-07 00:10:28 +0100

    Improve styling for menu

--- src/Components/Menu/Menu.styled.js
+++ src/Components/Menu/Menu.styled.js
@@ -1,9 +1,21 @@
 import styled from 'styled-components'
 
 const MenuList = styled.ul`
+    width: 90%;
+    padding-left: 0;
+    margin: 1rem 1.4rem;
+    border: 1px solid black;
     list-style-type: none;
-    padding-left: 1.4rem;
     text-align: left;
+    border-radius: 4px;
+
+    @media(min-width: 768px) {
+        width: 60%;
+    }
+    
+    @media(min-width: 1024px) {
+        width: 30%;
+    }
 `
 
 export {
