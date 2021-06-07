e0375e4e9996225c6a0abb51fdb9fbef80cefddd
AxSch <alex_schuneman@outlook.com>
2021-06-07 11:49:45 +0100

    Add setPreviousResults(), setIsSearch() & selectorFn()s

--- src/reducers/transportServices/transportServicesSlice.js
+++ src/reducers/transportServices/transportServicesSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const transportServices = createSlice({
     name: 'transportServices',
     initialState: {
-        bikeResults: {},
+        isSearch: false,
         previousBikeResults: {},
         currentTFLService: {}
 
@@ -11,12 +11,22 @@ const transportServices = createSlice({
     reducers: {
         setCurrentTFLService: (state, action) =>  {
             state.currentTFLService = action.payload
+        },
+        setPreviousResults: (state, action) => {
+            state.previousBikeResults = action.payload
+        },
+        setIsSearch: (state, action) => {
+            state.isSearch = action.payload
         }
     }
 })
 
 export const selectCurrentTFLService = (state) => state.transportServices.currentTFLService
 
-export const { setCurrentTFLService } = transportServices.actions
+export const selectPreviousBikeResults = (state) => state.transportServices.previousBikeResults
+
+export const selectIsSearch = (state) => state.transportServices.isSearch
+
+export const { setCurrentTFLService, setPreviousResults, setIsSearch } = transportServices.actions
 
 export default transportServices.reducer
